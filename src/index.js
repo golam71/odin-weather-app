33c42f73e2e3ca422157d578b76bce78fc204c1d
golam71 <74001687+golam71@users.noreply.github.com>
2025-06-09 16:27:12 +0600

    :bug: Fix error handling in showData function and revert default city to "Amazon"

--- src/index.js
+++ src/index.js
@@ -4,28 +4,33 @@ import { addCard } from "./javascript/card.js";
 import { loadBackground } from "./javascript/loadbg.js";
 
 async function showData(cityName) {
-  const json = await getData(cityName);
+  try {
+    const json = await getData(cityName);
 
-  document.getElementById("location").innerText = json.resolvedAddress;
-  document.getElementById("description").innerHTML = json.description;
-  loadBackground(json.resolvedAddress + " " + json.description);
+    document.getElementById("location").innerText = json.resolvedAddress;
+    document.getElementById("description").innerHTML = json.description;
+    loadBackground(json.resolvedAddress + " " + json.description);
 
-  const cards = document.getElementsByClassName("cards")[0];
-  cards.innerHTML = "";
+    const cards = document.getElementsByClassName("cards")[0];
+    cards.innerHTML = "";
 
-  for (const day of json.days) {
-    addCard({
-      icon: day.icon,
-      date: day.datetime.slice(-2),
-      temp: day.temp,
-      feelsLike: day.feelslike,
-      humidity: day.humidity,
-      description: day.description,
-    });
+    for (const day of json.days) {
+      addCard({
+        icon: day.icon,
+        date: day.datetime.slice(-2),
+        temp: day.temp,
+        feelsLike: day.feelslike,
+        humidity: day.humidity,
+        description: day.description,
+      });
+    }
+  } catch (error) {
+    alert("Something went wrong, try again.");
+    await showData("Amazon");
   }
 }
 
-await showData("hokaido");
+await showData("Amazon");
 
 document.getElementById("submit").addEventListener("click", async function () {
   const inputValue = document.querySelector("input").value || "Tokyo";
